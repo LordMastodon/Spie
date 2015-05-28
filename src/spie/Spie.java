00eba1645511f428fbf41c1350e08da99f63c4c5
Your Face <gsapiran@gmail.com>
2015-05-28 17:45:43 -0400

    Initial commit

---
+++ src/spie/Spie.java
@@ -0,0 +1,18 @@
+package spie;
+/**
+ * Created by student on 5/28/15.
+ */
+public class Spie extends JFrame {
+
+    //Game vision: A space game where you formalize the most powerful pie.
+
+    private void addComponentsToPane() {
+        JPanel panel = new JPanel();
+
+    }
+
+    public Spie(String name) {
+        super(name);
+    }
+
+}
