d01a151b7cd8064d570281b14439bf2c7160f3bf
Your Face <gsapiran@gmail.com>
2015-06-01 10:40:01 -0400

    Updated Stuff

--- src/main/java/spie/Spie.java
+++ src/main/java/spie/Spie.java
@@ -5,16 +5,22 @@ import org.apache.batik.swing.svg.GVTTreeBuilderAdapter;
 import org.apache.batik.swing.svg.GVTTreeBuilderEvent;
 import org.apache.batik.swing.svg.SVGDocumentLoaderAdapter;
 import org.apache.batik.swing.svg.SVGDocumentLoaderEvent;
+import spie.graphics.SpieComponent;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Spie {
     //Game vision: A space game where you formalize the most powerful pie.
+
+    public ArrayList<SpieComponent> components = new ArrayList();
 
     public static void main(String[] args) {
         JFrame f = new JFrame("Spie");
@@ -33,6 +39,11 @@ public class Spie {
 
     public Spie(JFrame f) {
         frame = f;
+        frame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                updateComponentPositions();
+            }
+        });
     }
 
     public JComponent createComponents() {
@@ -82,5 +93,11 @@ public class Spie {
         });
 
         return panel;
+    }
+
+    public void updateComponentPositions() {
+        for(int i = 0; i < components.size(); i++) {
+            components.get(i).setLocation(components.get(i).getPreferredPosition().getX(), components.get(i).getPreferredPosition().getY());
+        }
     }
 }
